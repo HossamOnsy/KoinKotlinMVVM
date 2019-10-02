0b96a2416fe44c3d0ebaf6b1aeba7d29f8f0e6fb
HossamOnsy <hossameldeenonsy@yahoo.com>
2019-10-03 00:09:18 +0200

    Altered

--- app/src/main/java/com/example/koinkotlinmvvm/networking/CatRepository.kt
+++ app/src/main/java/com/example/koinkotlinmvvm/networking/CatRepository.kt
@@ -8,7 +8,7 @@ class CatRepository(private val catApi: CatApi) {
     fun getCatList(): Response<List<Cat>>? {
         /*
          We try to return a list of cats from the API
-         Await the result from web service and then return it, catching any error from API
+         Get the result from web service and then return it
          */
         return catApi.getCats(limit = 30).execute()
     }
