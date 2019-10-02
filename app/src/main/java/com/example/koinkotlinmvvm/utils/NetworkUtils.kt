f4a309b4e46563e914ce6b217d6bb4f5a693b22f
HossamOnsy <hossameldeenonsy@yahoo.com>
2019-10-03 00:10:40 +0200

    Altered

--- app/src/main/java/com/example/koinkotlinmvvm/utils/NetworkUtils.kt
+++ app/src/main/java/com/example/koinkotlinmvvm/utils/NetworkUtils.kt
@@ -17,6 +17,8 @@ object NetworkUtils {
         return client.addInterceptor(interceptor).build()
     }
 
+    // this method creates CatApi Object which allows us to connect with apis and get our data from server as JSON
+    // GsonConverterFactory is what will parse this JSON into our Cat Object
     fun createWebService(): CatApi {
         val retrofit = Retrofit.Builder()
             .baseUrl(CAT_API_BASE_URL)
